8ca8b1fd28dd82c072b8b84c154dad95bc77380c
Khalid Yaseen <khalidty@gmail.com>
2022-03-16 04:17:57 +0000

    Added second tag

--- main.tf
+++ main.tf
@@ -137,10 +137,8 @@ resource "azurerm_virtual_machine" "catapp" {
 
  # tags = {}
 tags = { 
-  Department = "Billable"
+  Department , Billable
 }
-
-
   # Added to allow destroy to work correctly.
   depends_on = [azurerm_network_interface_security_group_association.catapp-nic-sg-ass]
 }
